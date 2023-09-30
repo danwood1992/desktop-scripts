8be5c449d2d1c82d11cd1653d3b0c5539df69ba8
Daniel Wood <gunnerwoody449@gmail.com>
2023-09-30 14:22:05 +0100

    chore: Edit logging.

--- git_setup.sh
+++ git_setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 source ./read-config.sh
+source ./utils.sh
+LOG_DIR="/var/log/my_dev_setup/packages"
+LOG_FILE="$LOG_DIR/install_packages.log"
 
+setup_logging
+
+log_entry "updating git config"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
+log_entry "git config updated"
 
 echo "Git configuration has been set up with the following values:"
 echo "Email: $GIT_EMAIL"
 echo "Name: $GIT_NAME"
+
+log_entry "Email Changed to: $GIT_EMAIL"
+log_entry "Name changed to: $GIT_NAME"
