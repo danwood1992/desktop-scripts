e4a69d6f33dfbddf143d4bf7d7b4ca395bc68314
Daniel Wood <gunnerwoody449@gmail.com>
2023-09-30 15:47:34 +0100

    chore: Remove sourcing.

--- system_info.sh
+++ system_info.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-source ./read-config.sh
 
-
-# Loop through the packages and check their versions
 for PACKAGE in "${PACKAGE_LIST[@]}"; do
     INSTALLED_VERSION=$(dpkg-query -W -f='${Version}' "$PACKAGE" 2>/dev/null)
     if [ -z "$INSTALLED_VERSION" ]; then
